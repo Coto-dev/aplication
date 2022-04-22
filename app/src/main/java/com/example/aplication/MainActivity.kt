132e64a9ace3f7cef532601dfd00a2b049915e17
coto1488 <shorop5@mail.ru>
2022-04-22 14:58:35 +0700

    update

--- app/src/main/java/com/example/aplication/MainActivity.kt
+++ app/src/main/java/com/example/aplication/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.aplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import com.example.aplication.databinding.ActivityMainBinding
@@ -11,14 +11,20 @@ class MainActivity : AppCompatActivity() {
     lateinit var blindingClass:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         blindingClass = ActivityMainBinding.inflate(layoutInflater)
 //        blindingClass - это основной контейнер где хранятся все эелементы с MainActivity
 //        пример работы blindingClass.beginButton.width = 100
-          setContentView(blindingClass.root)
+          setContentView(R.layout.activity_main)
     }
     fun OnClickStart(view: View){ // функция привязана к кнопке с id beginButton
         //val beginButton = findViewById<Button>(R.id.beginButton)
         setContentView(R.layout.activity_second)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) finish()
+        return true
     }
 }
 //:)
--- app/src/main/java/com/example/aplication/SecondActivity.kt
+++ app/src/main/java/com/example/aplication/SecondActivity.kt
@@ -2,10 +2,17 @@ package com.example.aplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "Codeblock"
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) finish()
+        return true
     }
 }
