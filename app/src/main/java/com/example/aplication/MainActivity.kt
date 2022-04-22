2e7a968cb5797424787142dba934922524aa866f
coto1488 <shorop5@mail.ru>
2022-04-22 13:20:21 +0700

    Переход на другое активити

--- app/src/main/java/com/example/aplication/MainActivity.kt
+++ app/src/main/java/com/example/aplication/MainActivity.kt
@@ -2,13 +2,22 @@ package com.example.aplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import com.example.aplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var blindingClass:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        blindingClass = ActivityMainBinding.inflate(layoutInflater)
+//        blindingClass - это основной контейнер где хранятся все эелементы с MainActivity
+//        пример работы blindingClass.beginButton.width = 100
+          setContentView(blindingClass.root)
     }
-}
-class Cat(){
-
+    fun OnClickStart(view: View){ // функция привязана к кнопке с id beginButton
+        //val beginButton = findViewById<Button>(R.id.beginButton)
+        setContentView(R.layout.activity_second)
+    }
 }
