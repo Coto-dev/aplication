8029128b75d7c092e37938a1ec8134e5e6ee6e59
maulina2 <101575070+maulina2@users.noreply.github.com>
2022-04-30 13:11:11 +0700

    bottom sheet

--- app/src/main/java/com/example/aplication/SecondActivity.kt
+++ app/src/main/java/com/example/aplication/SecondActivity.kt
@@ -1,11 +1,35 @@
 package com.example.aplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
+
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val frame = findViewById<FrameLayout>(R.id.sheet)
+        val bottomSheetBehavior: BottomSheetBehavior<*> =
+            BottomSheetBehavior.from<View>(frame)
+
+
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+
+        bottomSheetBehavior.peekHeight = 200
+        bottomSheetBehavior.isHideable = false
+
+        bottomSheetBehavior.setBottomSheetCallback(object : BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {}
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {}
+        })
     }
+
 }
+
